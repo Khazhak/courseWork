fc792475d20acedffdc227a3aff60131451849c4
Khazhak <khazhak.galstyan@codefights.com>
2019-12-18 00:39:27 +0400

    cifar-10RA.1.0

---
+++ courseWork.py
@@ -0,0 +1,251 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+import random
+import PIL, PIL.ImageOps, PIL.ImageEnhance, PIL.ImageDraw
+
+def imgToTensor(img):
+    transforms.ToTensor()
+    return img
+
+def tensorToImg(tens):
+    tens = tens / 2 + 0.5
+    img = Image.new('RGB', (32, 32))
+    lst = []
+    for i in range(32):
+        for j in range(32):
+            lst.append((int(tens[0][i][j].item() * 256), int(tens[1][i][j].item() * 256), int(tens[2][i][j].item() * 256)))
+    img.putdata(lst)
+    return img
+
+
+########### Augmentations
+def ShearX(img, v):  # [-0.3, 0.3]
+    assert -0.3 <= v <= 0.3
+    if random.random() > 0.5:
+        v = -v
+    return img.transform(img.size, PIL.Image.AFFINE, (1, v, 0, 0, 1, 0))
+
+def ShearY(img, v):  # [-0.3, 0.3]
+    assert -0.3 <= v <= 0.3
+    if random.random() > 0.5:
+        v = -v
+    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, 0, v, 1, 0))
+
+def TranslateX(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    assert -0.45 <= v <= 0.45
+    if random.random() > 0.5:
+        v = -v
+    v = v * img.size[0]
+    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, v, 0, 1, 0))
+
+def TranslateY(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    assert -0.45 <= v <= 0.45
+    if random.random() > 0.5:
+        v = -v
+    v = v * img.size[1]
+    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, 0, 0, 1, v))
+
+def TranslateXAbs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    assert 0 <= v <= 10
+    if random.random() > 0.5:
+        v = -v
+    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, v, 0, 1, 0))
+
+def TranslateYAbs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    assert 0 <= v <= 10
+    if random.random() > 0.5:
+        v = -v
+    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, 0, 0, 1, v))
+
+def Rotate(img, v):  # [-30, 30]
+    assert -30 <= v <= 30
+    if random.random() > 0.5:
+        v = -v
+    return img.rotate(v)
+
+def AutoContrast(img, _):
+    return PIL.ImageOps.autocontrast(img)
+
+def Invert(img, _):
+    return PIL.ImageOps.invert(img)
+
+def Equalize(img, _):
+    return PIL.ImageOps.equalize(img)
+
+def Flip(img, _):  # not from the paper
+    return PIL.ImageOps.mirror(img)
+
+def Solarize(img, v):  # [0, 256]
+    assert 0 <= v <= 256
+    return PIL.ImageOps.solarize(img, v)
+
+def Posterize(img, v):  # [4, 8]
+    assert 4 <= v <= 8
+    v = int(v)
+    return PIL.ImageOps.posterize(img, v)
+
+def Posterize2(img, v):  # [0, 4]
+    assert 0 <= v <= 4
+    v = int(v)
+    return PIL.ImageOps.posterize(img, v)
+
+def Contrast(img, v):  # [0.1,1.9]
+    assert 0.1 <= v <= 1.9
+    return PIL.ImageEnhance.Contrast(img).enhance(v)
+
+def Color(img, v):  # [0.1,1.9]
+    assert 0.1 <= v <= 1.9
+    return PIL.ImageEnhance.Color(img).enhance(v)
+
+def Brightness(img, v):  # [0.1,1.9]
+    assert 0.1 <= v <= 1.9
+    return PIL.ImageEnhance.Brightness(img).enhance(v)
+
+def Sharpness(img, v):  # [0.1,1.9]
+    assert 0.1 <= v <= 1.9
+    return PIL.ImageEnhance.Sharpness(img).enhance(v)
+
+def Cutout(img, v):  # [0, 60] => percentage: [0, 0.2]
+    assert 0.0 <= v <= 0.2
+    if v <= 0.:
+        return img
+
+    v = v * img.size[0]
+    return CutoutAbs(img, v)
+
+def Identity(img, v):
+    return img
+
+###########
+
+def augment_list():
+    augList = [(Identity, 0., 1.0),
+        (ShearX, 0., 0.3),
+        (ShearY, 0., 0.3),
+        (TranslateX, 0., 0.45),
+        (TranslateY, 0., 0.45),
+        (Rotate, 0, 30),
+        (AutoContrast, 0, 1),
+        (Equalize, 0, 1),
+        (Solarize, 0, 256),
+        (Posterize, 4, 8),
+        (Color, 0.1, 1.9),
+        (Brightness, 0.1, 1.9),
+        (Sharpness, 0.1, 1.9)]
+    return augList
+
+def applyAugment(func, img, v):
+    newImg = tensorToImg(img)
+    newImg = func(newImg, v)
+    newImg = (transforms.functional.to_tensor(newImg) - 0.5) * 2
+    return newImg
+
+def applyRandomAugment(img, v):
+    ra = random.choice(augment_list())
+    intensity = ra[1] + v * (ra[2] - ra[1]) / 100.
+    return applyAugment(ra[0], img, intensity)
+
+def applyRAMultipleTimes(img, n, v):
+    for i in range(n):
+        img = applyRandomAugment(img, v)
+    return img
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+def cifar10UsingRA(learningRate, Momentum, epochNum, batchSize, RANum, RAV):
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=10,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+    
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr = learningRate, momentum = Momentum)
+
+    for epoch in range(epochNum):
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            for id in range(batchSize):
+                inputs[id] = applyRAMultipleTimes(inputs[id], RANum, RAV)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            if i % 100 == 0:
+                print(i)
+            
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        
+    totalCorrectness = int(100 * correct / total)
+    
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(batchSize):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    worstClassCorrectness = 100
+    bestClassCorrectness = 0
+
+    for i in range(10):
+        thisClassCorrectness = int(100 * class_correct[i] / class_total[i])
+        worstClassCorrectness = min(worstClassCorrectness, thisClassCorrectness)
+        bestClassCorrectness = max(bestClassCorrectness, thisClassCorrectness)
+        
+    return [totalCorrectness, bestClassCorrectness, worstClassCorrectness]
