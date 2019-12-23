236df34b6d038e1a543f98ad5ae66c3fb09f86a5
Khazhak <khazhak.galstyan@codefights.com>
2019-12-23 23:00:13 +0400

    bugfix

--- courseWork.py
+++ courseWork.py
@@ -178,12 +178,12 @@ def cifar10UsingRA(learningRate, Momentum, epochNum, batchSize, RANum, RAV):
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                               shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=10,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                              shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat',
